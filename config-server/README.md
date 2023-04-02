b003cd9da85a66b6e1e57f65c4df269542316f2f
Humberto Dias <hldtux@gmail.com>
2023-04-02 20:50:09 -0300

    Initial commit

---
+++ config-server/README.md
@@ -0,0 +1,6 @@
+# springcloud-config-server
+springcloud-config-server
+
+
+
+http://localhost:9988/cook-service/default
---
+++ cook-service/src/main/java/com/example/configclient/CookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookServiceApplication.class, args);
+	}
+
+}
