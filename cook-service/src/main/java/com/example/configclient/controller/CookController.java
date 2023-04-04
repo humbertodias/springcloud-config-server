91679175c04abe9508e97e70ad3a92d32ac96454
Humberto Dias <hldtux@gmail.com>
2023-04-04 12:03:36 -0300

    host name

--- cook-service/src/main/java/com/example/configclient/controller/CookController.java
+++ cook-service/src/main/java/com/example/configclient/controller/CookController.java
@@ -1,10 +1,15 @@
 package com.example.configclient.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -13,17 +18,21 @@ public class CookController {
 
 
     @Value("${cook.count}")
-    int cookCount;
+    private int cookCount;
 
     @Value("${spring.profiles.active:}")
     private String activeProfile;
 
+    private String hostName;
+
     @GetMapping
-    public String cook() {
-
-        return "Cook count: " + cookCount + " active profile: " + activeProfile;
-
+    public Map<String, Object> cook() {
+        return Map.of("cookCount", cookCount, "activeProfile", activeProfile, "hostName", hostName);
     }
 
+    @PostConstruct
+    private void init() throws UnknownHostException {
+        this.hostName = InetAddress.getLocalHost().getHostName();
+    }
 
 }
